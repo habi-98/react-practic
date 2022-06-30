95566bd980762e06d0b2c0935b9ac28ca8625a6b
Sandurov Harbi <habi.alonso98@gmail.com>
2022-06-30 16:09:13 +0600

    Added change

--- src/App.js
+++ src/App.js
@@ -6,28 +6,38 @@ import PostFilter from "./components/PostFilter"
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import {getPageCount} from './utils/pages'
 import axios from "axios";
 import PostServer from "./API/PostServer";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {usePagination} from "./hooks/usePagination";
 
 function App() {
     const [posts, setPosts] = useState([])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
-    const [totalCount, setTotalCount] = useState(0)
+    const [totalPage, setTotalPage] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
     const sortedAdnSearchedPosts = usePosts(posts, filter.sort, filter.query)
+
+
+    const pagesArray = usePagination(totalPage)
+
+
     const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-        const response = await PostServer.getAll()
+        const response = await PostServer.getAll(limit, page)
+        console.log(response.headers['x-total-count'])
         setPosts(response.data)
-        setTotalCount(response.headers['x-total-count'])
+        const totalCount = response.headers['x-total-count']
+        setTotalPage(getPageCount(totalCount, limit))
     })
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
     }
-
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
---
+++ src/hooks/usePagination.js
@@ -0,0 +1,12 @@
+import {useMemo} from "react";
+
+export const usePagination = (totalPage) => {
+    const result = useMemo(() => {
+        const pagesArray = []
+        for (let i = 0; i < totalPage; i++) {
+            pagesArray.push(i + 1)
+        }
+        return pagesArray
+    }, [totalPage])
+    return result
+}
